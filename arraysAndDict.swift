689ee795ec350f0daa08b1ba81202bbfdbe5450a
iamakkkhil <iamakhil1408@gmail.com>
2021-08-27 01:16:14 +0530

    Arrays and Dict added

---
+++ arraysAndDict.swift
@@ -0,0 +1,9 @@
+var car: [String] = ["BMW", "Audi", "Jeep"]
+
+var values: [Any] = ["Strings", 123, "1", 1.2345]
+
+print(car, values)
+
+var dict: [String: Int] = ["One": 1, "Two":2]
+
+print(dict)
